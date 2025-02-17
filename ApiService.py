303dd2283babf49c943a82d9829c1ffa32abf9c2
shrey802 <shreyashpingle752@gmail.com>
2025-02-17 19:10:17 +0530

    exit OTP

--- ApiService.py
+++ ApiService.py
@@ -82,6 +82,18 @@ class ApiService:
         except requests.exceptions.JSONDecodeError:
             print(f"Error decoding JSON response: {response.text}")
             return None
+        
+    def exitVehicle(self, parkingTicketID, otp):
+        url = "ticket-handler/otp-exit-ticket"
+        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + self.env_config.get_token()}
+        payload = json.dumps({"parkingTicketID": parkingTicketID, "exitOTP": otp})
+        try:
+            response = requests.post(BASE_URL + url, headers=headers, data=payload)
+            response.raise_for_status()
+            return self._get_json_response(response)
+        except requests.exceptions.RequestException as e:
+            print(f"Error exiting vehicle: {e}")
+            return None
 
 
 env_config = EnvConfig()
