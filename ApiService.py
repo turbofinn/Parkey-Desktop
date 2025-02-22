0210140cfa92b267485e909539b08758189a9414
shrey802 <shreyashpingle752@gmail.com>
2025-02-22 21:41:39 +0530

    changes in APIService

--- ApiService.py
+++ ApiService.py
@@ -45,7 +45,7 @@ class ApiService:
 
     def getVehicleDetails(self, vehicleNo):
         url = "customer-flow-handler/get-vehicle-details?vehicleNo="+vehicleNo
-        headers = {'Authorization': "Bearer "+ "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJncmFudF90eXBlIjoiYXV0aG9yaXphdGlvbi10b2tlbiIsInVzZXJUeXBlIjoiRU1QTE9ZRUUiLCJpc3MiOiJQYXJra2V5Iiwic3ViIjoiMWQyMzNhNzktOTcyYS00ZDA5LTk2MTktZDc0MTE5OGMwNDQwIiwianRpIjoiYTBmYzQ1MDYtMGIzNC00MzkwLTkxOTYtNTg4Njg4ZGVmNDhlIiwiaWF0IjoxNzM4OTEzMjAzLCJleHAiOjIwNTQyNzMyMDN9.gJZDoGrAqMt7oCZ-7urIR3DDu4jZQPt1tvVvmo5Hat0"}
+        headers = {'Authorization': "Bearer "+ self.env_config.get_token()}
         response = requests.get(BASE_URL + url, headers=headers)
         response_data = response.json()
         return response_data
@@ -54,8 +54,8 @@ class ApiService:
         # URL of the API endpoint
         url = "customer-flow-handler/create-customer"
         source = "EMPLOYEE_DESKTOP"
-        empID = "5ec10c00-7eff-48c9-ada3-bce66129246d"
-        headers = { "Content-Type": "application/json","Authorization": "Bearer "+ "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJncmFudF90eXBlIjoiYXV0aG9yaXphdGlvbi10b2tlbiIsInVzZXJUeXBlIjoiRU1QTE9ZRUUiLCJpc3MiOiJQYXJra2V5Iiwic3ViIjoiMWQyMzNhNzktOTcyYS00ZDA5LTk2MTktZDc0MTE5OGMwNDQwIiwianRpIjoiYTBmYzQ1MDYtMGIzNC00MzkwLTkxOTYtNTg4Njg4ZGVmNDhlIiwiaWF0IjoxNzM4OTEzMjAzLCJleHAiOjIwNTQyNzMyMDN9.gJZDoGrAqMt7oCZ-7urIR3DDu4jZQPt1tvVvmo5Hat0"}  
+        empID = self.env_config.get_token()
+        headers = { "Content-Type": "application/json","Authorization": "Bearer "+ self.env_config.get_token()}  
         payload = json.dumps({"source": source,"mobileNo": mobileNo,"vehicleNo": vehicleNo, "employeeID": empID})
         
         response = requests.post(BASE_URL + url, headers=headers, data=payload)
@@ -66,8 +66,9 @@ class ApiService:
 
     def confirmTicket(self, parkingTicketID):
         url = "ticket-handler/confirm-ticket"
-        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVHlwZSI6IkVNUExPWUVFIiwiZ3JhbnRfdHlwZSI6ImF1dGhvcml6YXRpb24tdG9rZW4iLCJpc3MiOiJQYXJra2V5Iiwic3ViIjoiMWQyMzNhNzktOTcyYS00ZDA5LTk2MTktZDc0MTE5OGMwNDQwIiwianRpIjoiZjgzNTc4NmUtNWY3Ny00YzRiLThkNDMtMmNjZjI5OWU4YzcyIiwiaWF0IjoxNzM5Nzk4MjUxLCJleHAiOjIwNTUxNTgyNTF9.Uxjvvd1xdlmAv7ili7V4gPtDpg01KYePcY37P7njoV4"}
-        payload = json.dumps({"parkingTicketID": parkingTicketID, "requestType": "CONFIRM_TICKET", "employeeID": "5ec10c00-7eff-48c9-ada3-bce66129246d"})
+        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + self.env_config.get_token()}
+        empID = self.env_config.get_token()
+        payload = json.dumps({"parkingTicketID": parkingTicketID, "requestType": "CONFIRM_TICKET", "employeeID": self.env_config.get_token()})
         try:
             response = requests.post(BASE_URL + url, headers=headers, data=payload)
             response.raise_for_status()
@@ -81,7 +82,7 @@ class ApiService:
 
     def otpExitTicket(self, otp, parkingTicketID):
         url = "ticket-handler/otp-exit-ticket"
-        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVHlwZSI6IkVNUExPWUVFIiwiZ3JhbnRfdHlwZSI6ImF1dGhvcml6YXRpb24tdG9rZW4iLCJpc3MiOiJQYXJra2V5Iiwic3ViIjoiMWQyMzNhNzktOTcyYS00ZDA5LTk2MTktZDc0MTE5OGMwNDQwIiwianRpIjoiZjgzNTc4NmUtNWY3Ny00YzRiLThkNDMtMmNjZjI5OWU4YzcyIiwiaWF0IjoxNzM5Nzk4MjUxLCJleHAiOjIwNTUxNTgyNTF9.Uxjvvd1xdlmAv7ili7V4gPtDpg01KYePcY37P7njoV4"}
+        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + self.env_config.get_token()}
         payload = json.dumps({"parkingTicketID": parkingTicketID, "exitOTP": otp})
         try:
             response = requests.post(BASE_URL + url, headers=headers, data=payload)
@@ -93,8 +94,9 @@ class ApiService:
     
     def exitTicket(self, parkingTicketID):
         url = "ticket-handler/exit-ticket"
-        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVHlwZSI6IkVNUExPWUVFIiwiZ3JhbnRfdHlwZSI6ImF1dGhvcml6YXRpb24tdG9rZW4iLCJpc3MiOiJQYXJra2V5Iiwic3ViIjoiMWQyMzNhNzktOTcyYS00ZDA5LTk2MTktZDc0MTE5OGMwNDQwIiwianRpIjoiZjgzNTc4NmUtNWY3Ny00YzRiLThkNDMtMmNjZjI5OWU4YzcyIiwiaWF0IjoxNzM5Nzk4MjUxLCJleHAiOjIwNTUxNTgyNTF9.Uxjvvd1xdlmAv7ili7V4gPtDpg01KYePcY37P7njoV4"}
-        payload = json.dumps({"parkingTicketID": parkingTicketID, "employeeID": "5ec10c00-7eff-48c9-ada3-bce66129246d"})
+        empID = self.env_config.get_token()
+        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + self.env_config.get_token()}
+        payload = json.dumps({"parkingTicketID": parkingTicketID, "employeeID": empID})
         try:
             response = requests.post(BASE_URL + url, headers=headers, data=payload)
             response.raise_for_status()
@@ -105,7 +107,7 @@ class ApiService:
     
     def parkingCharges(self, parkingTicketID):
         url = "ticket-handler/get-parking-charges"
-        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVHlwZSI6IkVNUExPWUVFIiwiZ3JhbnRfdHlwZSI6ImF1dGhvcml6YXRpb24tdG9rZW4iLCJpc3MiOiJQYXJra2V5Iiwic3ViIjoiMWQyMzNhNzktOTcyYS00ZDA5LTk2MTktZDc0MTE5OGMwNDQwIiwianRpIjoiZjgzNTc4NmUtNWY3Ny00YzRiLThkNDMtMmNjZjI5OWU4YzcyIiwiaWF0IjoxNzM5Nzk4MjUxLCJleHAiOjIwNTUxNTgyNTF9.Uxjvvd1xdlmAv7ili7V4gPtDpg01KYePcY37P7njoV4"}
+        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + self.env_config.get_token()}
         params = {
         'parkingTicketID': parkingTicketID  
         }
@@ -120,7 +122,7 @@ class ApiService:
 
     def parkingSpaceDetails(self, parkingSpaceID):
         url = "user-management/parking-space/fetch-parking-space-info/"+ parkingSpaceID
-        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVHlwZSI6IkVNUExPWUVFIiwiZ3JhbnRfdHlwZSI6ImF1dGhvcml6YXRpb24tdG9rZW4iLCJpc3MiOiJQYXJra2V5Iiwic3ViIjoiMWQyMzNhNzktOTcyYS00ZDA5LTk2MTktZDc0MTE5OGMwNDQwIiwianRpIjoiZjgzNTc4NmUtNWY3Ny00YzRiLThkNDMtMmNjZjI5OWU4YzcyIiwiaWF0IjoxNzM5Nzk4MjUxLCJleHAiOjIwNTUxNTgyNTF9.Uxjvvd1xdlmAv7ili7V4gPtDpg01KYePcY37P7njoV4"}
+        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + self.env_config.get_token()}
         
         try:
             response = requests.get(BASE_URL + url, headers=headers)
@@ -131,8 +133,8 @@ class ApiService:
             return None
     
     def employeeDetails(self):
-        url = "user-management/employee/fetch-employee-info/"+ "5ec10c00-7eff-48c9-ada3-bce66129246d"
-        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVHlwZSI6IkVNUExPWUVFIiwiZ3JhbnRfdHlwZSI6ImF1dGhvcml6YXRpb24tdG9rZW4iLCJpc3MiOiJQYXJra2V5Iiwic3ViIjoiMWQyMzNhNzktOTcyYS00ZDA5LTk2MTktZDc0MTE5OGMwNDQwIiwianRpIjoiZjgzNTc4NmUtNWY3Ny00YzRiLThkNDMtMmNjZjI5OWU4YzcyIiwiaWF0IjoxNzM5Nzk4MjUxLCJleHAiOjIwNTUxNTgyNTF9.Uxjvvd1xdlmAv7ili7V4gPtDpg01KYePcY37P7njoV4"}
+        url = "user-management/employee/fetch-employee-info/"+ self.env_config.get_token()
+        headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + self.env_config.get_token()}
         
         try:
             response = requests.get(BASE_URL + url, headers=headers)
